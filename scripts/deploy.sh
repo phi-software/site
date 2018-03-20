310897158d808df559e0928fff29976e696ed738
philipe <felihenrique@gmail.com>
2018-03-20 12:29:54 -0300

    mod script deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,7 @@
+git clone https://github.com/felisoftware/felisoftware.github.io public
 cp static/* public/
 hexo generate
-mkdir public
 cd public
-git init
-git remote add origin https://github.com/felisoftware/felisoftware.github.io
 git add .
 git commit -m "deploy"
-git push origin master --force
+git push origin master
